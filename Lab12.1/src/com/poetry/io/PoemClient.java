1d14e5271ce0e9e388804bcaa6bdf29c32d1c870
Clayton Yan <claytony@tlgcohort.com>
2024-06-04 09:39:36 -0500

    poem

--- Lab12.1/src/com/poetry/io/PoemClient.java
+++ Lab12.1/src/com/poetry/io/PoemClient.java
@@ -8,8 +8,7 @@
 
 package com.poetry.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 
 public class PoemClient {
 
@@ -18,7 +17,7 @@ public class PoemClient {
      */
     public static void main(String[] args) {
         // readPoem();
-        // writePoem();
+        writePoem();
     }
 
     /**
@@ -35,7 +34,12 @@ public class PoemClient {
      */
     private static void readPoem() {
         // TODO: initialize 'reader' variable and complete the try block
-        try (BufferedReader reader = null) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("famous-poem.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // when it goes null, you are at End of file
+                System.out.println(line);
+            }
 
         }
         catch (IOException e) {
@@ -56,5 +60,14 @@ public class PoemClient {
      */
     private static void writePoem() {
         // TODO
+        try (PrintWriter writer = new PrintWriter(new FileWriter("haiku.txt"))) {
+            writer.println("Try IntelliJ");
+            writer.println("will be very fun said Jay");
+            writer.println("compile error");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
